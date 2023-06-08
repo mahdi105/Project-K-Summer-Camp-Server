65609cdebb859b0b86f1c443b84940aa604acf90
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-06-09 00:49:06 +0600

    Added mongodb configuration in the express server

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+// 
 
 // Middleware 
 app.use(cors());
@@ -16,11 +19,35 @@ app.use((req, res, next) => {
     next();
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.910rauf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+        // Send a ping to confirm a successful connection
+        client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 // Home API
 app.get('/', (req, res) => {
     res.send('Wellcome to Summer Camp Server');
 })
-
 // Port
 app.listen(port, () => {
     console.log(`The server is running on the port: ${port}`);

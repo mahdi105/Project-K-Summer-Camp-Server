9af6e46e1b477913a395a0fe91d5744cd94a7a0f
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-06-09 00:30:06 +0600

    Created express and and imported required package

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// Middleware 
+app.use(cors());
+app.use(express.json());// Request body parser
+
+// // Enable CORS == Solve the proble 'Browser stop the fetch request or unable to fetch
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });

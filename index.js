a2226b71cf66d5c5965870f18af025ec36cfd110
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-06-09 00:39:56 +0600

    Intial setupis completed and deployed into the vercel

--- index.js
+++ index.js
@@ -14,4 +14,14 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
+
+// Home API
+app.get('/', (req, res) => {
+    res.send('Wellcome to Summer Camp Server');
+})
+
+// Port
+app.listen(port, () => {
+    console.log(`The server is running on the port: ${port}`);
+})

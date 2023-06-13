3ed7123b78da6113d58ca7775c1a69bdae60d96e
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-06-13 10:17:27 +0600

    Created API for deleting a selected class by user

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
 
@@ -123,6 +123,13 @@ async function run() {
             }
             const query = {email: email};
             const result = await selectedClassesCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/selectedClass/:id', verifyJWT, async(req, res) => {
+            const id = req.params.id;
+            const filter = {id: new ObjectId(id)};
+            const result = await selectedClassesCollection.deleteOne(filter);
             res.send(result);
         })
 
